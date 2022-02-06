aaec01da9f07e5cd24a542637951d3d9cfe7939b
jo david <jo.wbdev@gmail.com>
2022-02-06 18:48:17 +0800

    fixing forgot pw

--- config/mail.php
+++ config/mail.php
@@ -36,7 +36,7 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
@@ -91,9 +91,17 @@ return [
     |
     */
 
+    'stream'=>[
+        'ssl'=>[
+            'allow_self_signed'=>true,
+            'verify_peer'=>false,
+            'verify_peer_name'=>false,
+        ],
+    ],
+
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'projectmcc01@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Forgot Password'),
     ],
 
     /*
