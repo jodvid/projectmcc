c34aba9b61dca9700bf86f06c882f2d3041d5366
jo david <jo.wbdev@gmail.com>
2022-02-06 14:11:06 +0800

    changing text size

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class=" relative flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <div class=" relative flex items-center justify-center dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden px-6 py-4 sm:block">
                     @auth
