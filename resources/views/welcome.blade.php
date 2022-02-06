a2b7f9c8b0f180431e2045a726f5e40c12cde40c
jo david <jo.wbdev@gmail.com>
2022-02-06 13:46:45 +0800

    changing text size

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -58,9 +58,9 @@
                                 
                                         
 
-                    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                    <footer class="ml-4 text-center text-sm text-gray-500 sm:text-center sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
-                    </div>
+                    </footer>
                 </div>
             </div>
         </div>
